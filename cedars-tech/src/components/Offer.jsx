5d9925156d3c39001fe6d1e30416f3f58641efcf
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-10 20:52:46 +0300

    Adding items

--- cedars-tech/src/components/Offer.jsx
+++ cedars-tech/src/components/Offer.jsx
@@ -1,73 +1,27 @@
 import React from "react";
-import { SiNetflix } from "react-icons/si";
-import { FaSpotify } from "react-icons/fa";
 import Anghami from "../assets/anghami-icon.svg";
 import Shahid from "../assets/shahid-icon.svg";
 import Osn from "../assets/osn-icon.svg";
+import Netflix from "../assets/netflix-icon.svg";
+import Item from "./Item";
+import { TbBrandDisney } from "react-icons/tb";
 
 const Offer = () => {
   return (
     <section className="flex flex-col items-center text-center gap-6 py-25">
       <h1 className="text-4xl font-bold">What We Offer</h1>
-      <p className="text-gray-600 max-w-xl">
+      <p className="text-gray-400 max-w-xl">
         Get all your favorite channels, movies, and live sports — for less.
       </p>
 
-      <div className="grid grid-cols-1 md:grid-cols-5 gap-8 mt-10 w-full max-w-6xl px-4">
-        <div className="border p-6 rounded-lg shadow hover:shadow-lg transition">
-          <div className="flex items-center justify-center mb-4">
-            <SiNetflix size={40} className="text-red-600" />
-          </div>
-          <h2 className="text-2xl font-semibold mb-2">Netflix</h2>
-          <p className="text-gray-700 mb-2 font-semibold">Prices:</p>
-          <ul className="text-gray-600">
-            <li>1 Month: $3</li>
-            <li>3 Months: $8</li>
-            <li>12 Months: $25</li>
-          </ul>
-        </div>
-
-        <div className="border p-6 rounded-lg shadow hover:shadow-lg transition">
-          <div className="flex items-center justify-center mb-4">
-            <img width={45} src={Anghami} alt="" />
-          </div>
-          <h2 className="text-2xl font-semibold mb-2">Anghami</h2>
-          <p className="text-gray-700 mb-2 font-semibold">Prices:</p>
-          <ul className="text-gray-600">
-            <li>1 Month: $3</li>
-            <li>3 Months: $8</li>
-            <li>12 Months: $25</li>
-          </ul>
-        </div>
-
-        <div className="border p-6 rounded-lg shadow hover:shadow-lg transition">
-          <div className="flex items-center justify-center mb-4">
-            <img width={80} src={Shahid} alt="" />
-          </div>
-          <h2 className="text-2xl font-semibold mb-2">Shahid VIP</h2>
-          <p className="text-gray-700 mb-2 font-semibold">Prices:</p>
-          <ul className="text-gray-600">
-            <li>1 Month: $3</li>
-            <li>3 Months: $8</li>
-            <li>12 Months: $25</li>
-          </ul>
-        </div>
-
-        <div className="border p-6 rounded-lg shadow hover:shadow-lg transition">
-          <div className="flex items-center justify-center mb-4">
-            <img width={50} src={Osn} alt="" />
-          </div>
-          <h2 className="text-2xl font-semibold mb-2">OSN Plus</h2>
-          <p className="text-gray-700 mb-2 font-semibold">Prices:</p>
-          <ul className="text-gray-600">
-            <li>1 Month: $3</li>
-            <li>3 Months: $8</li>
-            <li>12 Months: $25</li>
-          </ul>
-        </div>
-
+      <div className="flex lg:flex-row flex-col">
+        <Item imgUrl={Netflix} imgWidth="50"/>
+        <Item imgUrl={Anghami} imgWidth="50"/>
+        <Item imgUrl={Shahid} imgWidth="120"/>
+        <Item imgUrl={Osn} imgWidth="70"/>
         
       </div>
+      
     </section>
   );
 };
