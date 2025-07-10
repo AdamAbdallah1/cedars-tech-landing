30db2d210e4618d49e8c7eacdbcc8b29ac0c895a
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-10 20:52:32 +0300

    Creating item

--- cedars-tech/src/components/Item.jsx
+++ cedars-tech/src/components/Item.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const Item = () => {
+
+const Item = ({ title, imgUrl, imgWidth }) => {
   return (
-    <div className="gap-8 mt-10 w-full max-w-6xl px-4 flex flex-col lg:flex-row justify-around">
-        <div className="border p-6 rounded-lg shadow hover:shadow-lg transition">
+    <div className={`gap-8 mt-10 w-full max-w-6xl px-4 flex flex-col lg:flex-col justify-around`}>
+        <div className={`h-30 flex flex-col justify-center items-center p-2 rounded-lg shadow hover:shadow-lg border-4 border-neutral-800`}>
           <div className="flex items-center justify-center mb-4">
-            <SiNetflix size={40} className="text-red-600" />
+            <img width={imgWidth} src={imgUrl} alt={title} />
           </div>
-          <h2 className="text-2xl font-semibold mb-2">Netflix</h2>
         </div>
     </div>
   )
