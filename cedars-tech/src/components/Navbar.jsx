256b88e8c105d95bb09cecd139aa351c863dd25d
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-10 18:42:58 +0300

    Creating navbar with icons

--- cedars-tech/src/components/Navbar.jsx
+++ cedars-tech/src/components/Navbar.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { CiInstagram } from "react-icons/ci";
+import { CiFacebook } from "react-icons/ci";
+import { FaWhatsapp } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='w-full p-10 flex justify-between text-3xl items-center'>
+        <h1 className=''>Cedars Tech</h1>
+        <div className='flex gap-5 text-4xl'>
+            <CiInstagram />
+            <CiFacebook />
+            <FaWhatsapp />
+        </div>
+    </nav>
   )
 }
 
