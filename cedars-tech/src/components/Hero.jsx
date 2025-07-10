743df078be4155215d07c29311f7370413e620ee
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-10 19:05:47 +0300

    Changing button colors

--- cedars-tech/src/components/Hero.jsx
+++ cedars-tech/src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
         <h1 className='text-5xl font-inter'>Affordable <span className='bg-gradient-to-r from-pink-300 via-slate-500 to bg-purple-500 bg-clip-text tracking-tight text-transparent'>Streaming</span> Services Made Simple</h1>
         <p>Cedars Tech helps you enjoy premium streaming at the best price</p>
         <div>
-             <button className='bg-indigo-700 p-4 rounded-2xl hover:bg-indigo-900 cursor-pointer'><a href="">Contact Us on WhatsApp</a></button>
+             <button className='bg-[#9754DE] text-neutral-900 font-bold p-4 rounded-2xl hover:bg-[#F5A2D1] cursor-pointer'><a href="">Contact Us on WhatsApp</a></button>
         </div>
     </div>
   )
