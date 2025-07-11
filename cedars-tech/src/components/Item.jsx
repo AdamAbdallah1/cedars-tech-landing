a7c860cf25fe031dd1ced82a0a9dc13ed0d434b9
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-11 17:49:45 +0300

    Changing styles for items inside Offer page

--- cedars-tech/src/components/Item.jsx
+++ cedars-tech/src/components/Item.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const Item = ({ title, imgUrl, imgWidth }) => {
   return (
-    <div className={`gap-8 mt-10 w-full max-w-6xl px-4 flex flex-col lg:flex-col justify-around`}>
-        <div className={`h-30 flex flex-col justify-center items-center p-2 rounded-lg shadow hover:shadow-lg border-4 border-neutral-800`}>
-          <div className="flex items-center justify-center mb-4">
+    <div className={`gap-8 mt-10 w-full max-w-6xl px-4 flex justify-center items-center flex-row`}>
+        <div className={`h-30 w-30 flex flex-row justify-center items-center p-4 rounded-lg shadow hover:shadow-lg border-4 border-neutral-800`}>
+          <div className="flex flex-row items-center justify-center mb-4">
             <img width={imgWidth} src={imgUrl} alt={title} />
           </div>
         </div>
