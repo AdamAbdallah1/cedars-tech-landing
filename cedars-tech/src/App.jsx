a993279de43e370dd253b19b91fe4e0bb84a35a0
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-11 17:52:33 +0300

    Adding Contact page

--- cedars-tech/src/App.jsx
+++ cedars-tech/src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Offer from './components/Offer'
+import Contact from './components/Contact'
 
 function App() {
   
@@ -8,7 +12,10 @@ function App() {
       <div className='fixed top-0 -z-10 h-full w-full'>
       <div class="relative h-full w-full bg-slate-950"><div class="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div><div class="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div></div>      </div>
       <div className='container mx-auto px-8 '>
-        
+        <Navbar />
+        <Hero />
+        <Offer />
+        <Contact />
       </div>
     </div>
   )
