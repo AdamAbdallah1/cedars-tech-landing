1ecf1b3814bec304a6461379ff9ca14bd470c31c
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-12 19:47:32 +0300

    Fixing sizes

--- cedars-tech/src/components/Contact.jsx
+++ cedars-tech/src/components/Contact.jsx
@@ -19,12 +19,12 @@ const Contact = () => {
         whileInView={{ opacity: 1, y: 0 }}
         initial={{ opacity: 0, y: -100 }}
         transition={{ duration: 0.5 }}
-        className="lg:text-6xl text-3xl  font-bold">Contact us</motion.h1>
+        className="lg:text-5xl text-3xl  font-bold">Contact us</motion.h1>
         <motion.p 
         whileInView={{ opacity: 1, x: 0 }}
         initial={{ opacity: 0, x: -100 }}
         transition={{ duration: 1 }}
-        className="lg:text-3xl text-gray-400 max-w-xl">
+        className="lg:text-2xl text-gray-400 max-w-xl">
             Ready to start? Chat with us now!
         </motion.p>
         <div>
@@ -33,13 +33,13 @@ const Contact = () => {
         initial={{ opacity: 0, y: -100 }}
         transition={{ duration: 0.5 }}
             className='flex lg:flex-row flex-col gap-8'>
-                <button className='bg-[#9754DE] text-neutral-900 font-bold lg:p-6 p-4 lg:text-2xl rounded-2xl hover:bg-red-300 cursor-pointer'>
+                <button className='bg-[#9754DE] text-neutral-900 font-bold lg:p-5 p-4 lg:text-2xl rounded-2xl hover:bg-red-300 cursor-pointer'>
                   <a className='flex justify-center items-center gap-2' href="">
                     Contact Us on <FaWhatsapp size={30}/>
                   </a>
                 </button>
 
-                <button className='bg-[#9754DE] text-neutral-900 font-bold lg:p-6 p-4 lg:text-2xl rounded-2xl hover:bg-red-300cursor-pointer'>
+                <button className='bg-[#9754DE] text-neutral-900 font-bold lg:p-6 p-4 lg:text-2xl rounded-2xl hover:bg-red-300 cursor-pointer'>
                   <a className='flex justify-center items-center gap-2' href="">
                     Contact Us on <FaInstagram size={30}/>
                   </a>
