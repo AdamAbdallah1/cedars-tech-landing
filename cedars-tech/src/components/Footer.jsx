6dd0afe9be010bbb3c76c3fa6c3f25fc0c889d7b
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-12 17:38:18 +0300

    Creating footer with icons

--- cedars-tech/src/components/Footer.jsx
+++ cedars-tech/src/components/Footer.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { CiInstagram } from "react-icons/ci";
+import { FaWhatsapp } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="text-white text-center p-4 pt-20">
+      © {new Date().getFullYear()} Cedars Tech. All rights reserved.
+      <div className='flex justify-evenly pt-5'>
+        <CiInstagram size={30} />
+        <FaWhatsapp size={30}/>
+        <FaFacebook size={30}/>
+        <FaTiktok size={30}/>
+      </div>
+    </footer>
   )
 }
 
