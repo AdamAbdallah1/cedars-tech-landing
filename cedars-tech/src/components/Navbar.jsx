18f03c9aa8112d9c5fe9b2395e3408613d6084e4
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-12 21:48:46 +0300

    Fixing sizes

--- cedars-tech/src/components/Navbar.jsx
+++ cedars-tech/src/components/Navbar.jsx
@@ -5,8 +5,8 @@ import { FaWhatsapp } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <nav className='w-full p-10 flex justify-between text-3xl items-center'>
-        <h1 className=''>Cedars Tech</h1>
+    <nav className='w-full p-10 flex gap-30 justify-center text-3xl items-center'>
+        <h1 className='text-1xl'>Cedars Tech</h1>
         <div className='flex gap-5 text-4xl'>
             <a className='hover:text-gray-400' href="https://www.instagram.com/cedars.tech/"><CiInstagram /></a>
             <a className='hover:text-gray-400' href="#"><CiFacebook /></a>
