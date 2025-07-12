498fd6a52625d4dac2c550c57524628727bfc3f6
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-12 18:23:10 +0300

    Removing icons

--- cedars-tech/src/components/Footer.jsx
+++ cedars-tech/src/components/Footer.jsx
@@ -1,19 +1,13 @@
 import React from 'react'
-import { CiInstagram } from "react-icons/ci";
+import { FaInstagram } from "react-icons/fa";
 import { FaWhatsapp } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import { FaTiktok } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="text-white text-center p-4 pt-20">
+    <footer className="text-white text-center p-5 pt-20 lg:text-2xl flex flex-col gap-5">
       © {new Date().getFullYear()} Cedars Tech. All rights reserved.
-      <div className='flex justify-evenly pt-5'>
-        <CiInstagram size={30} />
-        <FaWhatsapp size={30}/>
-        <FaFacebook size={30}/>
-        <FaTiktok size={30}/>
-      </div>
     </footer>
   )
 }
