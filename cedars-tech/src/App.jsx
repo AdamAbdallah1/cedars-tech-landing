eca70da3666bbd2b90fccee0727678f93ff57cb2
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-09-28 22:31:38 +0300

    Importing SplashCursor

--- cedars-tech/src/App.jsx
+++ cedars-tech/src/App.jsx
@@ -5,20 +5,19 @@ import Hero from './components/Hero'
 import Offer from './components/Offer'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
-import Pricing from './components/Pricing'
+import SplashCursor from './components/SpalshCursor'
 
 function App() {
   
   return (
     <div className='min-h-screen overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
       <div className='fixed top-0 -z-10 h-full w-full'>
-      <div class="relative h-full w-full bg-slate-950"><div class="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div><div class="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div></div>      </div>
+      <div class="relative h-full w-full bg-slate-950"></div>      </div>
       <div className='container mx-auto px-8 '>
+        <SplashCursor />
         <Navbar />
         <Hero />
         <Offer />
-        <Pricing />
-
         <Contact />
         <Footer />
       </div>
